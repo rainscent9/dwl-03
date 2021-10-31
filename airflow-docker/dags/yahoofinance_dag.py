b9dce27d3d553dff79a2629a9b9c4f555b33ff08
rainscent9 <82468008+rainscent9@users.noreply.github.com>
2021-10-31 23:50:39 +0100

    yahoofinance dag

--- airflow-docker/dags/yahoofinance_dag.py
+++ airflow-docker/dags/yahoofinance_dag.py
@@ -1,38 +1,83 @@
+'''
+Description:
+This dag pulls periodically stock prices from YahooFinance API
+and stores it in a defined Postgres database.
+
+Postgres handling from airflow:
+Write:      PostgresOperator
+Read:       PostgresHook
+
+Info, structure:
+The SQL commands are stored in folder sql to make the code more readable.
+
+Resources:
+https://marclamberti.com/blog/postgres-operator-airflow/
+https://www.nylas.com/blog/use-python-requests-module-rest-apis/
+https://www.yahoofinanceapi.com/tutorial
+'''
+
 import datetime
 import logging
+import requests
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators.postgres_operator import PostgresOperator
-from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.postgres_operator import PostgresOperator # Write to Postgres
+from airflow.hooks.postgres_hook import PostgresHook # Read from Postgres
 
+response_list=[]
+response_dict=dict()
 
 def start():
-    logging.info('Starting the DAG')
+    logging.info('Starting the DAG YahooFinance AIP')
 
-def get_records():
-    request = "SELECT * FROM test"
+def get_data_api():
+    url = "https://yfapi.net/v6/finance/quote?"
+
+    querystring = {'symbols': 'CS',  # if many: 'symbols':'AAPL,BTC-USD,EURUSD=X'
+                   'region': 'US',
+                   'lang': 'en-US'}
+
+    headers = {
+        'accept': 'application/json',
+        'x-api-key': 'TZ1z5Ndk7010iPfALDXa15jAIuLnzLVD2DSFik9e'
+    }
+
+    try:
+        response = requests.get(url, headers=headers, params=querystring, timeout=5)
+        response.raise_for_status()
+        # Code here will only run if the request is successful
+        print('The request was a success!')
+        print(response.json())
+    except requests.exceptions.HTTPError as errh:
+        print('HTTP Error occured')
+    except requests.exceptions.ConnectionError as errc:
+        print('Connection Error occured')
+    except requests.exceptions.Timeout as errt:
+        print('Timeout Error occured')
+    except requests.exceptions.RequestException as err:
+        print('Request Exception Error occured')
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    r = response.json()['quoteResponse']['result']
+    response_dict = r[0]
+    response_list = list(response_dict.values())
+
+def insert_values():
+    logging.info('Write values into data base')
+    request='sql/insert_values_yahoofin.sql'
+    parameters = response_dict
     pg_hook = PostgresHook(postgres_conn_id="datalake_eldorado", schema="datalake1")
     connection = pg_hook.get_conn()
     cursor = connection.cursor()
+    # cursor.execute(request, parameters)
     cursor.execute(request)
-    sources = cursor.fetchall()
-    for source in sources:
-        logging.info(source)
-    return sources
-
-def get_pandas():
-    db_hook = PostgresHook(postgres_conn_id="datalake_eldorado", schema="datalake1")
-    df = db_hook.get_pandas_df('SELECT * FROM test')
-    logging.info(f'Successfully used PostgresHook to return {len(df)} records')
-    logging.info(df)
-
 
 dag = DAG(
-        'yahoofinance',
-        schedule_interval='@hourly',
-        start_date=datetime.datetime.now() - datetime.timedelta(days=1))
-
+    'yahoofinance',
+    schedule_interval='@hourly',
+    start_date=datetime.datetime.now() - datetime.timedelta(days=1)
+)
 
 greet_task = PythonOperator(
    task_id="start_task",
@@ -44,111 +89,22 @@ create_table = PostgresOperator(
     task_id="create_table",
     dag=dag,
     postgres_conn_id="datalake_eldorado",
-    sql='''
-        CREATE TABLE IF NOT EXISTS yahoofin (
-            id int,
-            language text,
-            region text,
-            quoteType text,
-            quoteSourceName text,
-            triggerable bool,
-            currency text,
-            postMarketChangePercent int,
-            postMarketTime int,
-            postMarketPrice int,
-            postMarketChange int,
-            regularMarketChange int,
-            regularMarketChangePercent int,
-            regularMarketTime int,
-            regularMarketPrice int,
-            regularMarketDayHigh int,
-            regularMarketDayRange text,
-            regularMarketDayLow int,
-            regularMarketVolume int,
-            regularMarketPreviousClose int,
-            bid int,
-            ask int,
-            bidSize int,
-            askSize int,
-            fullExchangeName text,
-            financialCurrency text,
-            regularMarketOpen int,
-            averageDailyVolume3Month int,
-            averageDailyVolume10Day int,
-            fiftyTwoWeekLowChange int,
-            fiftyTwoWeekLowChangePercent int,
-            fiftyTwoWeekRange text,
-            fiftyTwoWeekHighChange int,
-            fiftyTwoWeekHighChangePercent int,
-            fiftyTwoWeekLow int,
-            fiftyTwoWeekHigh int,
-            dividendDate int,
-            trailingAnnualDividendRate int,
-            trailingPE int,
-            trailingAnnualDividendYield int,
-            epsTrailingTwelveMonths int,
-            marketState text,
-            exchange text,
-            longName text,
-            messageBoardId text,
-            exchangeTimezoneName text,
-            exchangeTimezoneShortName text,
-            gmtOffSetMilliseconds int,
-            market text,
-            esgPopulated bool,
-            epsForward int,
-            sharesOutstanding int,
-            bookValue int,
-            fiftyDayAverage int,
-            fiftyDayAverageChange int,
-            fiftyDayAverageChangePercent int,
-            twoHundredDayAverage int,
-            twoHundredDayAverageChange int,
-            twoHundredDayAverageChangePercent int,
-            marketCap int,
-            forwardPE int,
-            priceToBook int,
-            sourceInterval int,
-            exchangeDataDelayedBy int,
-            averageAnalystRating text,
-            tradeable bool,
-            firstTradeDateMilliseconds int,
-            priceHint int,
-            shortName text,
-            symbol text
-        )
-    '''
+    sql='sql/create_table_yahoofin.sql'
+)
+
+get_data_api = PythonOperator(
+    task_id='get_data_api',
+    python_callable=get_data_api,
+    dag=dag
 )
 
 insert_values = PostgresOperator(
     task_id="insert_values",
-    dag=dag,
     postgres_conn_id="datalake_eldorado",
-    sql='''
-            INSERT INTO yahoofin VALUES (
-            ?,?,?,?,?,?,?,?,?,?,
-            ?,?,?,?,?,?,?,?,?,?,
-            ?,?,?,?,?,?,?,?,?,?,
-            ?,?,?,?,?,?,?,?,?,?,
-            ?,?,?,?,?,?,?,?,?,?,
-            ?,?,?,?,?,?,?,?,?,?,
-            ?,?,?,?,?,?,?,?,?
-            )
-        '''
-)
-
-get_records = PythonOperator(
-    task_id="get_records",
-    python_callable=get_records,
-    dag=dag
+    dag=dag,
+    sql='sql/insert_values_yahoofin.sql',
+    parameters=response_dict
 )
 
 
-get_pandas = PythonOperator(
-    task_id='get_pandas',
-    python_callable=get_pandas,
-    dag=dag
-    )
-
-
-greet_task >> create_table >> insert_values >> get_records >> get_pandas
+greet_task >> create_table >> get_data_api >> insert_values
